38c33d66587095192c8d4933dd02fe58eb70c80e
YamHorin <horinyam@gmail.com>
2023-10-06 14:32:49 +0300

    Changes to be committed: 	modified:   Makefile 	modified:   omp_MPI_functions.c 	modified:   omp_MPI_functions.h

--- omp_MPI_functions.c
+++ omp_MPI_functions.c
@@ -10,72 +10,31 @@
 #define MAX_STRING_SIZE 3000
 
 
-
-int caculate_result_without_matrix(const char* first_str, const char *s2, int off_set, int k)
-{
-    int length = strlen(s2);
-    int result = 0;
-#pragma omp parallel for reduction(+ : result)
-    for (int i = 0; i < length; i++)
-    {
-        if (i >= k)
-            result = *((first_str + i) + off_set) == toupper(*((s2 + i)) + 1);
-        else
-        {
-            if (*((first_str + i) + off_set) == s2[i])
-                result++;
-        }
-    }
-    return result;
-}
-
-int calculate_result_with_matrix(const char* first_str, const char *s2, int matrix[MATRIX_SIZE][MATRIX_SIZE], int off_set, int k)
-{
-    int length = strlen(s2);
-    int result = 0;
-
-#pragma omp parallel for reduction(+ : result)
-    for (int i = 0; i < length; i++)
-    {
-
-        char c1 = *(first_str + i + off_set);
-        char c2 = *(s2 + i);
-        c1 = toupper(c1);
-        c2 = toupper(c2);
-        int x = c1 - 'A';
-        int y = c2 - 'A'; 
-        if (i >= k)
-        {
-            y++;
-            if (c2 == 'Z')
-                y = 0;
-        }
-
-        if (x >= 0 && x < MATRIX_SIZE && y >= 0 && y < MATRIX_SIZE) // Check bounds
-        {
-            result += matrix[x][y];
-        }
-    }
-    return result;
-}
-
-//to fix
 void caculate_max_score_no_grade_table(char* str_to_check , char* first_str , struct score_alignment* AS_ptr)
 {
     int lenght_first_str = strlen(first_str);
     int size_str_to_check = strlen(str_to_check);
     int sqn_taries = (size_str_to_check < lenght_first_str) ? (lenght_first_str - size_str_to_check)
                                                         : (size_str_to_check - lenght_first_str);
-    int off_set, max_off_set, max_score = 0;
+    int off_set, max_score = 0;
     int k, max_k, score;
-    
+    #pragma omp parallel private(k ,off_set ,max_score)
     for (off_set = 0; off_set <= sqn_taries; off_set++)
     {   
-
-        
         for (k = 0; k < size_str_to_check; k++)
         {
-
+            #pragma omp for reduction(+ : score)
+            for (int i = 0; i < size_str_to_check; i++)
+            {
+                if (i >= k)
+                    score = (*((first_str + i) + off_set) == toupper(*((str_to_check + i)) + 1));
+                else
+                {
+                    if (*((first_str + i) + off_set) == str_to_check[i])
+                        score++;
+                }
+            }
+            #pragma omp critical
             if (max_score <= score)
             {
                 AS_ptr->K = k;
